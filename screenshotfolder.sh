86e001b0d5a35f3ffbf7981a617edf70b755e389
Trevor Ward <78617817+TrevorW-code@users.noreply.github.com>
2022-01-19 14:18:48 -0700

    Update screenshotfolder.sh
    Needed to add a reminder to restart your machine, because sometimes it does not work right away.

--- screenshotfolder.sh
+++ screenshotfolder.sh
@@ -4,4 +4,6 @@ read varname
 echo Okay, creating screenshot folder $varname
 mkdir ~/Desktop/$varname
 defaults write com.apple.screencapture location ~/Desktop/$varname
+killall SystemUIServer
 echo $varname created. All screenshots will now populate this folder.
+echo If change does not take effect, please restart your machine.
